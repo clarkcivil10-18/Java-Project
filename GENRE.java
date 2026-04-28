63c275ce234f84ec2b4e7f2605ffb4299c7920c8
Bryan <clarkcivil10@gmail.com>
2026-04-28 14:31:24 -0400

    change

---
+++ GENRE.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class GENRE - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum GENRE
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
