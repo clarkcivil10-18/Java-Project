c0e86df314acf8f3e79e55531757fd6e241d99ff
Bryan <clarkcivil10@gmail.com>
2026-05-11 23:34:15 -0400

    ASDSA

--- Main.java
+++ Main.java
@@ -7,7 +7,9 @@ public class Main
      * Constructor for objects of class Main
      */
     public static void main(String[] arg)
+    
     {
+        Scanner scanner = new Scanner(System.in);
         GameLibrary library = new GameLibrary("My Library");
         Game game1 = new Game("Elden Ring", Genre.RPG,AgeRating.M,Platform.PC);
         Game game2 = new Game("Minecraft", Genre.ACTION,AgeRating.E10,Platform.CONSOLE);
@@ -21,6 +23,21 @@ public class Main
         library.addGame(game4);
         library.addGame(game5);
         library.listAllGames();
+        boolean running = true; 
+        
+        while(running)
+        {
+            System.out.println("--- Library Menu ---");
+            System.out.println("1. Add Game");
+            System.out.println("2. List All Games");
+            System.out.println("3. List By Genre");
+            System.out.println("4. List By Rating");
+            System.out.println("5. Exit");
+            System.out.println("-------------------------");
+            System.out.print("Enter choice: ");
+            int choice = scanner.nextInt(); 
+            
+        }
     }
     
     
